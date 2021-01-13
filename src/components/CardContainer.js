5614251bfb7392a1e92916a069e4ae7d10e912b8
Muriel Jakazi <murielnjakazi@gmail.com>
2021-01-13 18:48:36 +0100

    axios model in place

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 import Card from './Card';
+import axios from 'axios';
+
 
 function CardContainer() {
-    const [potions, SetPotions]= useState([])
+    const [potions, setPotions]= useState([]);
 
+    const getPotions = () => {
+        axios.get('http://localhost:5000/potions')
+        .then(response => response.data)
+        .then(data => {
+            setPotions(data.results[0])
+        })
+    }
     return (
         <div className="card-container">
             {potions.map((potion, index) => {
