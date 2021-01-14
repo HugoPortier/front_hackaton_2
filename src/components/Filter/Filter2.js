d246184d77fafae7ec23bb332e93a83aab7e90c3
Muriel Jakazi <murielnjakazi@gmail.com>
2021-01-14 20:30:47 +0100

    filters 1 and 2 functioning

--- src/components/Filter/Filter2.js
+++ src/components/Filter/Filter2.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Select } from 'semantic-ui-react'
 
-const countryOptions = [
-  { key: 'af', value: 15, text: 15 },
-  { key: 'ax', value: 30, text: 30 },
-  { key: 'al', value: 60, text: 60 },
+const ageOptions = [
+  { key: 'all', value: null, text: 'Afficher tout' },
+  { key: 'af', value: 15, text: '15' },
+  { key: 'ax', value: 30, text: '30' },
+  { key: 'al', value: 60, text: '60' },
 ]
 
 function SelectExample(props) {
 
   const handleChange = (e) => {
-    props.filter(e.target.innerText);
+    const selectedOption = ageOptions.filter(option => option.text === e.target.innerText)[0];
+    props.filter(selectedOption.value);
   }
   
   return(
-    <Select placeholder='Filter by age' options={countryOptions} onChange={(e) => handleChange(e)} />
+    <Select placeholder='Filter by age' options={ageOptions} onChange={(e) => handleChange(e)} />
   )
 }
 
