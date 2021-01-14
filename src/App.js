328e24fb32d32dcac0bbb311e751421f95bc8398
HugoStewart06 <hugoportier3@gmail.com>
2021-01-14 13:07:51 +0100

    add a browser router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import CardContainer from './components/CardContainer';
 import Footer from'./components/Footer/Footer';
-
-
+import Panier from './components/Panier/Panier'
 
 import './App.css';
 
@@ -14,11 +14,21 @@ function App() {
 
   return (
     <div className="App">
+      <Switch>
 
+        <Route exact path='/'>
         <Navbar />
         <Banner />
         <CardContainer />
         <Footer />
+        </Route>
+
+        <Route exact path='/panier'>
+        <Panier/>
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
