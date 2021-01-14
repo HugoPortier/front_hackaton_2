3562e861df349e98622e9eee8faf8c7701791205
HugoStewart06 <hugoportier3@gmail.com>
2021-01-14 23:23:14 +0100

    add button delete wip

--- src/components/Panier/Panier.js
+++ src/components/Panier/Panier.js
@@ -2,15 +2,33 @@ import React,{ useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Panier = () => {
+
     const [panier, setPanier] = useState([])
-console.log("panier", panier);
-useEffect(() => {
+    const [exterminate, setExterminate] = useState([])
+
+    console.log("panier", panier);
+
+    useEffect(() => {
     axios.get(`http://localhost:5000/panier`)
     .then(res => {console.log(res);
         setPanier(res.data)
     })
     
 }, [])
+
+
+const delete_panier = (id) => {
+    axios.delete(`http://localhost:5000/panier/${id}`)
+    .then(res => {
+        setPanier(res.data[0])
+    })
+}
+useEffect(() => {
+    axios.get(`http://localhost:5000/panier`)
+    .then(res => {console.log(res);
+    })
+    
+}, [exterminate])
 
     return (
         <div>
@@ -19,7 +37,10 @@ useEffect(() => {
                     <div>
                         <img src={item.image} key={item.id} alt={item.nom}/>
                         <p>{item.nom}</p>
-                        <p>{item.effet}</p>
+                        <p>{item.prix}</p>
+                        <button 
+                        onClick={()=> delete_panier(item.id)}
+                        >X</button>
                     </div>
                 )
 
@@ -28,4 +49,5 @@ useEffect(() => {
     )
 }
 
-export default Panier
+export default Panier;
+
