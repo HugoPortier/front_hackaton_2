f2a05e69189dfb4bb858668e134d1b0b4d55d8c9
HugoStewart06 <hugoportier3@gmail.com>
2021-01-14 13:57:14 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         </Route>
 
         <Route exact path='/panier'>
+        <Navbar/>
         <Panier/>
         </Route>
 
--- src/components/Panier/Panier.js
+++ src/components/Panier/Panier.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function panier() {
+const panier = () => {
     return (
         <div>
             <h1>
